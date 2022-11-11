e8826e7d28d6645d6c0724267f0843d35e7b4f93
Mullakhmetov Ilshat <ilshat1331@yandex.ru>
2022-11-12 00:21:29 +0500

    init di

--- lib/app/ui/main_app_runner.dart
+++ lib/app/ui/main_app_runner.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/widgets.dart';
+import 'package:it_client/app/di/init_di.dart';
 import 'package:it_client/app/domain/app_builder.dart';
 import 'package:it_client/app/domain/app_runner.dart';
 
 class MainAppRunner implements AppRunner {
+  final String env;
+
+  const MainAppRunner(this.env);
+
   @override
   Future<void> preloadData() async {
     WidgetsFlutterBinding.ensureInitialized();
     // init app
-    // init di
+    initDi(env);
     // init config
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:it_client/app/ui/main_app_builder.dart';
 import 'package:it_client/app/ui/main_app_runner.dart';
 
 void main() {
-  final runner = MainAppRunner();
+  const env = String.fromEnvironment("env", defaultValue: "dev");
+  const runner = MainAppRunner(env);
   final builder = MainAppBuilder();
   runner.run(builder);
 }
